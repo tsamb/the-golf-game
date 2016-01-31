0ee1f87aa098bca0ddeda2030c1113d6fa1cc403
Sam Blackman <sblackman@gmail.com>
2016-02-01 00:25:32 +1300

    set up basic tests for players and pairs

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Pair, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    xit 'cannot have more than two players' do
+
+    end
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'relationships' do
+    let(:player) { Player.create!(name: "Sam") }
+    let(:partner1) { Player.create!(name: "Charlie") }
+    let(:partner2) { Player.create!(name: "Milly") }
+
+    it 'can have many pairs' do
+      player.pairs << partner1
+      player.pairs << partner2
+      expect(player.pairs.count).to eq(2)
+    end
+
+    xit 'has many partners via pairing' do
+
+    end
+  end
+
+  describe 'constraints' do
+    xit 'must be created with a name' do
+
+    end
+  end
 end
